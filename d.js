96684b6f30d8fa29bed3c85eb228ccb9242ce538
AdrianFeijoFagundes <134751683+AdrianFeijoFagundes@users.noreply.github.com>
2024-10-21 12:42:20 -0300

    Create d.js

---
+++ d.js
@@ -0,0 +1,14 @@
+// é possivel nomear variaveis com $ e _
+
+const camelCase = true;
+const snake_case = true;
+
+// tipos de dados
+
+// Number
+// String
+// Boolean
+// Undefined
+// null
+// Symbol
+// BigInt
